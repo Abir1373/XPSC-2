8e5da1a5abf47666b80c8d48ff4ca696f8bffe6c
Abir1373 <abirm3188@gmail.com>
2025-02-14 23:44:55 +0600

    14.02.2025

---
+++ W14/Extra/C_Postcard.cpp
@@ -0,0 +1,130 @@
+// Bismillahir Rahmanir Raheem
+
+// author : (عبد) Dewan Abir Mahmud 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std ;
+
+#define f first
+#define s second
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define B begin
+#define rb rbegin
+#define E end
+#define re rend
+#define bs binary_search
+#define ub upper_bound
+#define lb lower_bound
+#define tc() long long int test_case ; cin >> test_case ; while(test_case--)
+#define cout(a) cout << a << endl
+#define all(v) sort(v.begin(),v.end())
+#define rall(v) sort(v.rbegin(),v.rend())
+#define rev(v) reverse(v.begin(),v.end())
+#define sz(s) s.size() ;
+#define Optimize ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0)
+#define inf INT_MAX ;
+#define clr(x,y) memset(x,y,sizeof x)
+#define pii pair<ll,ll>
+#define vll vector<ll>
+#define vpi vector<pii>
+
+template <typename T> using o_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    string s ; cin >> s ; 
+    ll k ; cin >> k ; 
+    ll snow = 0 , cane = 0 , tot = 0 , snow_i = 0 ; 
+    ll n = sz(s) ; 
+    for (ll i=0;i<n;i++)
+    {
+        if (s[i]=='?')cane++ ; 
+        else if (s[i]=='*')
+        {
+            snow++ ; 
+            snow_i = i ; 
+        } 
+        else tot++ ; 
+    }
+    if (k > tot)
+    {
+        if (snow_i==0)
+        {
+            cout << "Impossible\n" ; 
+            return ; 
+        }
+        string ans = "" , tmp = "" ; 
+        ll d = k - tot ; 
+        while(d--)
+        {
+            tmp += s[snow_i-1] ; 
+        } 
+        for (ll i=0;i<n;i++)
+        {
+            if(i==snow_i)
+            {
+                ans += tmp ; 
+            }
+            else if (s[i]!='?' && s[i]!='*')
+            {
+                ans += s[i] ; 
+            }
+        }
+        cout << ans << '\n' ; 
+    }
+    else if (k==tot)
+    {
+        for (ll i=0;i<n;i++)
+        {
+            if (s[i]!='?' && s[i]!='*')
+            {
+                cout << s[i] ;
+            }
+        }
+        cout << "\n" ; 
+    }
+    else 
+    { 
+        if (tot-k>cane+snow)
+        {
+            cout << "Impossible\n" ; 
+            return ; 
+        }
+        ll mn = tot - k ; 
+        string ans = "" ; 
+        for (ll i=0;i<n;i++)
+        {
+            if (s[i]=='?' || s[i]=='*')
+            {
+                continue ;
+            }
+            if (s[i+1]=='?' || s[i+1]=='*')
+            {
+                if (mn)
+                {
+                    mn-- ; 
+                }
+                else ans+= s[i] ; 
+            }
+            else ans+=s[i] ;
+        }
+
+        cout << ans << '\n' ;
+    }
+}
+
+signed main()
+{
+   Optimize;
+//    tc()
+    solve();
+}
