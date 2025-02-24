87c416f7a5e6864d62285a00f2376f4a35f0319a
Abir1373 <abirm3188@gmail.com>
2025-02-24 22:10:56 +0600

    24.02.2025

---
+++ W16/Extra/B_Difference_of_GCDs.cpp
@@ -0,0 +1,70 @@
+// Bismillahir Rahmanir Raheem
+
+// author : Dewan Abir Mahmud 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std ;
+
+#define f first
+#define s second
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define B begin
+#define rb rbegin
+#define E end
+#define re rend
+#define bs binary_search
+#define ub upper_bound
+#define lb lower_bound
+#define tc() long long int test_case ; cin >> test_case ; while(test_case--)
+#define cout(a) cout << a << endl
+#define all(v) sort(v.begin(),v.end())
+#define rall(v) sort(v.rbegin(),v.rend())
+#define rev(v) reverse(v.begin(),v.end())
+#define sz(s) s.size() ;
+#define Optimize ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0)
+#define inf INT_MAX ;
+#define clr(x,y) memset(x,y,sizeof x)
+#define pii pair<ll,ll>
+#define vll vector<ll>
+#define vpi vector<pii>
+
+template <typename T> using o_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    ll n , a , b ; cin >> n >> a >> b ;
+    vll ans ;  
+    for (ll i=1;i<=n;i++)
+    {
+        ll d = a/i ;
+        if (a%i)d++ ; 
+        ll p = d * i ; 
+        if (p>b)
+        {
+            cout << "NO\n" ; 
+            return ;
+        }
+        ans.pb(p) ; 
+    }
+    cout << "YES\n" ; 
+    for (ll i=0;i<n;i++)
+    {
+        cout << ans[i] << " \n" [i==n-1] ; 
+    }
+}
+
+signed main()
+{
+   Optimize;
+   tc()
+    solve();
+}
