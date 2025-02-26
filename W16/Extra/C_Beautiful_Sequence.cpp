d19cd2ce305b43ba364e2866ec4051b611e88152
Abir1373 <abirm3188@gmail.com>
2025-02-26 23:00:17 +0600

    26.02.2025

---
+++ W16/Extra/C_Beautiful_Sequence.cpp
@@ -0,0 +1,83 @@
+// Bismillahir Rahmanir Raheem
+
+// author : Dewan Abir Mahmud 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std ;
+
+#define f first
+#define s second
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define B begin
+#define rb rbegin
+#define E end
+#define re rend
+#define bs binary_search
+#define ub upper_bound
+#define lb lower_bound
+#define tc() long long int test_case ; cin >> test_case ; while(test_case--)
+#define cout(a) cout << a << endl
+#define all(v) sort(v.begin(),v.end())
+#define rall(v) sort(v.rbegin(),v.rend())
+#define rev(v) reverse(v.begin(),v.end())
+#define sz(s) s.size() ;
+#define Optimize ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0)
+#define inf INT_MAX ;
+#define clr(x,y) memset(x,y,sizeof x)
+#define pii pair<ll,ll>
+#define vll vector<ll>
+#define vpi vector<pii>
+
+template <typename T> using o_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll mod = 998244353 ; 
+
+ll add(ll a,ll b)
+{
+    a += b ; 
+    while(a>mod)
+    {
+        a -= mod ; 
+    }
+    if (a<0) a+=mod ; 
+    return a ; 
+}
+
+void solve()
+{
+    ll n ; cin >> n ;
+    vll dp(4,0) ; 
+    for (ll i=0,p;i<n;i++)
+    {
+        cin >> p ; 
+        if (p==1)
+        {
+            dp[1] = add(dp[1],1ll) ; 
+        }
+        else if (p==2)
+        {
+            dp[2] = add(dp[2], add (dp[1],dp[2])) ; 
+        }
+        else 
+        {
+            dp[3] = add (dp[3],dp[2]) ; 
+        }
+    }
+    cout << dp[3] << '\n' ;
+}
+
+signed main()
+{
+   Optimize;
+   tc()
+    solve();
+}
---
+++ W16/Extra/D_For_Wizards_the_Exam_Is_Easy_but_I_Couldn_t_Handle_It.cpp
@@ -0,0 +1,80 @@
+// Bismillahir Rahmanir Raheem
+
+// author : Dewan Abir Mahmud 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std ;
+
+#define f first
+#define s second
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define ll long long int
+#define ld long double
+#define nl '\n'
+#define B begin
+#define rb rbegin
+#define E end
+#define re rend
+#define bs binary_search
+#define ub upper_bound
+#define lb lower_bound
+#define tc() long long int test_case ; cin >> test_case ; while(test_case--)
+#define cout(a) cout << a << endl
+#define all(v) sort(v.begin(),v.end())
+#define rall(v) sort(v.rbegin(),v.rend())
+#define rev(v) reverse(v.begin(),v.end())
+#define sz(s) s.size() ;
+#define Optimize ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0)
+#define inf INT_MAX ;
+#define clr(x,y) memset(x,y,sizeof x)
+#define pii pair<ll,ll>
+#define vll vector<ll>
+#define vpi vector<pii>
+
+template <typename T> using o_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    ll n ; cin >> n ;
+    vll v(n) ; 
+    ll l = 1 , r = 1 , ans = 0 ; 
+    for (ll i=0;i<n;i++)
+    {
+        cin >> v[i] ; 
+    }
+    for (ll i=0;i<n-1;i++)
+    {
+        ll sm = 0 , bg = 0 ; 
+        for (ll j=i+1;j<n;j++)
+        {
+            if (v[j]>v[i])
+            {
+                bg++ ; 
+            }
+            else if (v[j]<v[i])
+            {
+                sm++ ; 
+            }
+            if ((sm-bg)>ans)
+            {
+                ans = sm - bg ; 
+                l = i+1 ;
+                r = j+1 ; 
+            }
+        }
+    }
+    cout << l << ' ' << r << '\n' ; 
+}
+
+signed main()
+{
+   Optimize;
+   tc()
+    solve();
+}
